e664aa370a321a7feb739ccf88ae50d098e61012
Patrick Da Silva <patrick1dasilva@gmail.com>
2023-05-24 01:07:24 +0200

    Simplified ShadowBox component

--- src/components/generic/ShadowBox/ShadowBox.tsx
+++ src/components/generic/ShadowBox/ShadowBox.tsx
@@ -1,5 +1,4 @@
 // Third-party imports
-import type { FC } from "react"
 
 // Global imports
 
@@ -8,20 +7,12 @@ import { StyledContainer } from "./styles"
 
 ////////////////////////////////////////////////////////////////////////////////
 
-interface ShadowBoxProps {
-	children?: any
-}
-
 /**
  * @function ShadowBox
  * @category Components
  * @subcategory Generic
  * @description Used for containers that render content potentially anywhere on the website.
  */
-const ShadowBox: FC<ShadowBoxProps> = ({ children }) => (
-	<StyledContainer>
-		{children}
-	</StyledContainer>
-)
+const ShadowBox = StyledContainer
 
 export default ShadowBox
