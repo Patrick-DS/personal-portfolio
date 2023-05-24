4d8fe41ad4bdb3e16f7e9e6aca64ccd2348d0259
Patrick Da Silva <patrick1dasilva@gmail.com>
2023-05-24 17:35:49 +0200

    Added missing margin on StyledMain for the footer

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -18,5 +18,5 @@ export const StyledMain = styled.main`
 	max-width: 1200px;
 	height: 100%;
 	min-height: 100vh;
-	margin: ${({ theme }) => theme.dimensions.header.height} auto 0 auto;
+	margin: ${({ theme }) => theme.dimensions.header.height} auto ${({ theme }) => theme.dimensions.footer.height} auto;
 `
