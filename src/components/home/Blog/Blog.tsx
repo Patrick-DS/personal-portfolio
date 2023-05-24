43a20a09b326243c22abe4ea428e024726e46b08
Patrick Da Silva <patrick1dasilva@gmail.com>
2023-05-24 16:34:42 +0200

    Added headline to blog page

--- src/components/home/Blog/Blog.tsx
+++ src/components/home/Blog/Blog.tsx
@@ -4,6 +4,8 @@ import type { FC } from "react"
 // Global imports
 
 // Local imports
+import BlogCard from "../BlogCard"
+import { BlogContainer, BlogTitle, BlogSubtitle } from "./styles"
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -17,6 +19,16 @@ interface BlogProps {
  * @subcategory Home
  * @description Renders the blog content of the homepage.
  */
-const Blog: FC<BlogProps> = ({ children }) => children
+const Blog: FC<BlogProps> = ({ children }) => (
+	<BlogContainer>
+		<BlogCard>
+			<BlogTitle>Patrick Da Silva</BlogTitle>
+			<BlogSubtitle>Your reference in software, cloud and machine learning</BlogSubtitle>
+		</BlogCard>
+		<BlogCard>
+			{children}
+		</BlogCard>
+	</BlogContainer>
+)
 
 export default Blog
---
+++ src/components/home/Blog/styles.ts
@@ -0,0 +1,45 @@
+// Third-party imports
+import styled from "styled-components"
+
+// Global imports
+
+// Local imports
+
+////////////////////////////////////////////////////////////////////////////////
+
+export const BlogContainer = styled.div`
+	width: 100%;
+	display: grid;
+	margin-top: 100px;
+	gap: 50px;
+`
+
+export const BlogTitle = styled.h1`
+	/* Positioning */
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	
+	/* Dimensions */
+	width: 100%;
+
+	/* Text */
+	font-size: 36px;
+	font-weight: 400;
+	text-align: center;
+`
+
+export const BlogSubtitle = styled.h2`
+	/* Positioning */
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	
+	/* Dimensions */
+	width: 100%;
+
+	/* Text */
+	font-size: 30px;
+	font-weight: 400;
+	text-align: center;
+`
